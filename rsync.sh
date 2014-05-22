7bfd1b05d392bdd2ce9488f611ecb1e29d7f8b3c
Owen Pragel <owenxg@gmail.com>
2014-05-22 16:12:58 -0700

    Changed sync direction to server -> client, now uses mount_afp not open. Requires terminal.

--- rsync.sh
+++ rsync.sh
@@ -2,20 +2,32 @@
 
 lastUser=`defaults read /Library/Preferences/com.apple.loginwindow lastUserName`
 
-serverAddress=file.jehunw.com
-serverFolder=Science
+serverAddress='file.jehunw.com'
+serverDirectory='Science'
 
-open afp://$serverAddress/$serverFolder
-sleep 10
-if [ ! -d /Volumes/$serverFolder ]; then
-	echo "Could not connect to the file server!"
+ping -c 1 $serverAddress
+if [ $? != 0 ]; then
+	echo "Could not connect to the file server. Are you on the office network or VPN?"
 	exit 1
+fi
+
+mkdir -p /Volumes/$serverDirectory
+mountPoint=/Volumes/$serverDirectoy
+
+# Alternate AFP command, calls mount_afp.
+#open afp://$serverAddress/$serverFolder
+mount_afp -i afp://$serverAddress/$serverDirectory $mountPoint
+
+if [ $? != 0 ]; then
+	echo "Could not authenticate/connect to the file server! If you continue to experience problems please contact an administrator."
+	exit 2
 else
-	rsync -av /Users/$lastUser/Documents/Secure\ Documents/Literature/Shared\ Library/ /Volumes/Science/Papers/Shared\ Library/
+	rsync -av "/Volumes/Science/Papers/Shared Library/" "/Users/$lastUser/Documents/Secure Documents/Literature/Shared Library/"
 	if [ $? == 0 ]; then
 		echo "Transfer complete!"
 	else
 		echo "Transfer failed!"
+		exit 3
 	fi
 fi
 
