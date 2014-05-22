9ae2a7af77550a6c996c43f8f3646cd9ce2f2d7d
Owen Pragel <owenxg@gmail.com>
2014-05-22 16:40:17 -0700

    Removed specific servers/directories, added placeholders.

--- rsync.sh
+++ rsync.sh
@@ -2,37 +2,35 @@
 
 lastUser=`defaults read /Library/Preferences/com.apple.loginwindow lastUserName`
 
-serverAddress='file.jehunw.com'
-serverDisk='Science'
+serverAddress='example'
+serverDisk='example'
 # Path below intended to be absolute - see rsync line
-serverDirectory='/Papers/Shared Library/'
+serverDirectory='example'
 
 ping -c 1 $serverAddress
 if [ $? != 0 ]; then
-	echo "Could not connect to the file server. Are you on the office network or VPN?"
+	echo "Could not connect to the file server $serverAddress. Are you on the office network or VPN?"
 	exit 1
 fi
 
 mkdir -p /Volumes/$serverDisk
 mountPoint=/Volumes/$serverDisk
-localDirectory="/Users/$lastUser/Documents/Secure Documents/Literature/Shared Library/"
+localDirectory='example'
 
 # Alternate AFP command, calls mount_afp.
 #open afp://$serverAddress/$serverFolder
 mount_afp -i afp://$serverAddress/$serverDisk $mountPoint
 
 if [ $? != 0 ]; then
-	echo "Could not authenticate/connect to the file server! If you continue to experience problems please contact an administrator."
-	
+	echo 'Could not authenticate/connect to the file server! If you continue to experience problems please contact an admin.'
 	exit 2
 else
 	rsync -av "$mountPoint$serverDirectory" "$localDirecory"
 	if [ $? == 0 ]; then
-		echo "Transfer complete!"
+		echo 'Transfer complete!'
 	else
-		echo "Transfer failed!"
+		echo 'Transfer failed!'
 		exit 3
 	fi
 fi
-
 exit 0 
